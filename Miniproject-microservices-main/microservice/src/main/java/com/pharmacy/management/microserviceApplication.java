51700e70a44a8e5f7498bb6cc1456aa4ae13e569
Sanjaybalasubramaniyan <113581738+Sanjaybalasubramaniyan@users.noreply.github.com>
2023-07-27 09:32:08 +0530

    Add files via upload

---
+++ Miniproject-microservices-main/microservice/src/main/java/com/pharmacy/management/microserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacy.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class microserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(microserviceApplication.class, args);
+	}
+
+}
